8377ca3dc51ec485cfe958509939b9cf70625cb4
yevgeny.nikitin <yevgeny.nikitin@simbirsoft.com>
2025-01-21 14:17:42 +0300

    creating footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import * as React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 w-full h-12 md:h-16 text-neutral-500 px-4 lg:px-20 xl:px-40 bg-gray-300 uppercase font-bold flex justify-between items-center  ">
+      <Link className="text-neutral-500 text-xl" href="/">
+        AXOLOTL
+      </Link>
+      <p>© ALL RIGHTS RESERVED</p>
+    </footer>
+  );
+};
+
+export default Footer;
